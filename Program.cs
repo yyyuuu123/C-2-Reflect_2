bac0f0341fe5260138bf9ed394c0a33603fc41c6
NikitaJe <yyyuuu123q@gmail.com>
2024-02-07 18:40:47 +1000

    dfdsfs

--- Program.cs
+++ Program.cs
@@ -7,36 +7,28 @@ GetTypeObject(new Car());
 GetTypeObject(new Book());
 GetTypeObject(new Empl());
 
-static void GetTypeObject<T>(T data)
-{
-	Type typeOfObject = null;
-	object mainDataUser = null;
-
-	//Get type object
-	switch (data)
-	{
 		case Book:
-			typeOfObject = typeof(Book);
-			break;
-		case Car:
-			typeOfObject = typeof(Car);
-			break;
-		case Empl:
-			typeOfObject = typeof(Empl);
-			break;
-		default:
-			break;
-	}
+	typeOfObject = typeof(Book);
+	break;
+case Car:
+	typeOfObject = typeof(Car);
+	break;
+case Empl:
+	typeOfObject = typeof(Empl);
+	break;
+default:
+	break;
+}
 
-	//Get fields
-	PropertyInfo?[] fieldInfos = typeOfObject.GetProperties();
+//Get fields
+PropertyInfo?[] fieldInfos = typeOfObject.GetProperties();
 
-	foreach (PropertyInfo item in fieldInfos)
-	{
-		System.Console.WriteLine($"Enter: {item.Name}");
-		string inputUser = Console.ReadLine();
-		object? changeValue = Convert.ChangeType(inputUser, item.PropertyType);
-		item.SetValue(data, changeValue);
-	}
-	System.Console.WriteLine();
+foreach (PropertyInfo item in fieldInfos)
+{
+	System.Console.WriteLine($"Enter: {item.Name}");
+	string inputUser = Console.ReadLine();
+	object? changeValue = Convert.ChangeType(inputUser, item.PropertyType);
+	item.SetValue(data, changeValue);
+}
+System.Console.WriteLine();
 }
