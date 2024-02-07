45fe932a2d21295871b636058953c76ffee65bf0
NikitaJe <yyyuuu123q@gmail.com>
2024-02-07 19:47:03 +1000

    test

--- Models/Book.cs
+++
@@ -1,6 +0,0 @@
-class Book
-{
-	public int Year { get; set; }
-	public string Author { get; set; }
-	public string Genre { get; set; }
-}
--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using C_2_Reflect_2;
-using C_2_Reflect_2.Models;
-
-
-UITools.FillAnyModel(new Car());
